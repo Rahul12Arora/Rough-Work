63b735e448a12987fc23a643634d7460aab8b00c
Rahul Arora <rahul.012.arora@gmail.com>
2023-06-09 12:47:18 +0530

    upd

--- backend/index.js
+++ backend/index.js
@@ -11,17 +11,28 @@ const User = require('./models/userSchema');
 connectDB()
 
 // Handle GET request to fetch all users
-app.get('/users', async (req, res) => {
+app.get('/dashboard', async (req, res) => {
   try {
     const users = await User.find({});
-    res.json(users || 'NO DATA FOUND');
+    res.send(users)
+  } catch (err) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+app.get('/signin', async (req, res) => {
+  try {
+    const users = await User.find({email:req.body.email, password:req.body.password});
+    if(users.length > 0) { res.json(true)}
+    else{
+      res.json(false)
+    }
   } catch (err) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
 // Handle POST request to create a new user
-app.post('/users', async (req, res) => {
+app.post('/signup', async (req, res) => {
   try {
     const newUser = new User(req.body);
     const savedUser = await newUser.save();
